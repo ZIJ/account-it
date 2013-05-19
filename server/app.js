215e3a11bb0e0ab184763ee1dcd992265536659e
Paul Kozlovitch <Paul.Kozlovitch@gmail.com>
2013-05-19 15:46:01 +0300

    server updated

--- server/app.js
+++ server/app.js
@@ -1,23 +1,96 @@
 // Retrieve
+var mongoose = require('mongoose');
+var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-    if(!err) {
-        console.log("We are connected");
-    } else {
-        console.log(err);
-    }
-    var collection = db.collection('test');
-    var docs = [{
-        foo: 'bar1'
-    },{
-        foo: 'bar2'
-    }];
-    collection.insert(docs, {w: 1}, function(err, result){
-        console.log(result);
-        var all = collection.find().toArray(function(err, items){
-            console.log(items);
-        });
+var roomSchema = mongoose.Schema({
+    name: String,
+    operations: [{
+        name: String,
+        amount: Number,
+        contributions: [{
+            person: String,
+            paidAmount: Number,
+            ownsAmount: Number
+        }]
+    }]
+});
+
+var Room = mongoose.model('Room', roomSchema);
+
+
+mongoose.connect('mongodb://localhost:27017/accountItDb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log('success');
+});
+
+var app = express();
+app.use(express.bodyParser());
+
+app.get('/', function(req, res){
+    res.send('Account-it');
+});
+
+app.get('/roomIds', function(req, res){
+    console.log('all rooms requested');
+    Room.find().select('_id').exec(function(err, selection){
+        if (err) {
+            res.send(500);
+        } else {
+            res.send(selection.map(function(obj){
+                return obj._id;    
+            }));
+        }
     });
 });
+
+app.get('/room/:id', function(req, res){
+    Room.findById(req.params.id, function(err, room){
+        if (err){
+            res.send(404);
+        } else {
+            res.send(room);
+        }
+    });
+});
+
+app.post('/room/:id', function(req, res){
+    Room.findByIdAndUpdate(req.params.id, req.body, function(err, room){
+        if (err){
+            res.send(500);
+        } else {
+            res.send(room);
+        }
+    });
+});
+
+app.put('/room', function(req, res){
+    var room = new Room(req.body);
+    room.save(function(err){
+        if (err) {
+            res.send(500);
+        } else {
+            res.send(room);
+        }
+    });
+});
+
+app.del('/room/:id', function(req, res){
+    Room.findById(req.params.id, function(err, room){
+        if (err){
+            res.send(500);
+        } else {
+            room.remove(function(err, room){
+                if (err) {
+                    res.send(500);
+                } else {
+                    res.send();
+                }
+            });
+        }
+    });    
+});
+
+app.listen(3000);
