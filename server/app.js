6bee8cc9e190a2b988f205834a7ae8aa92e06e3c
Igor Zalutsky <igor.zalutsky@gmail.com>
2013-05-26 21:31:44 +0300

    app.'s cleanup and comments

--- server/app.js
+++ server/app.js
@@ -2,11 +2,18 @@
 
     'use strict';
 
+    /* Third-party dependencies */
+
     var mongoose = require('mongoose');
     var express = require('express');
     var http = require('http');
 
+    /* In-project dependencies */
+
     var config = require('./config');
+
+
+    /* Persistance */
 
     mongoose.connect(config.mongoUrl);
     var db = mongoose.connection;
@@ -15,17 +22,24 @@
         console.log('success');
     });
 
+
+    /* App stack initialization */
+
     var app = express();
 
     app.use(express.bodyParser());
     app.use(express.cookieParser());
-    app.use(express.session({ secret: 'greygoose' }));
+    app.use(express.session({ secret: 'greygoose' }));  //TODO move secret to config
     app.use(express.static(config.staticDir));
 
+
+    /* API endpoints */
 
     require('./apis/roomApi')(app);
     require('./apis/authApi')(app);
 
+
+    /* Server startup */
 
     http.createServer(app).listen(config.port, function(){
         console.log("Account-it 2.0 listening on " + config.port);
